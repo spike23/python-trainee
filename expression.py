d8da959a2d463a88afe6d23c974b5ffbaf149019
spike23 <nickolay.osiniy@gmail.com>
2016-04-02 17:15:26 +0300

    площадь и периметр

---
+++ expression.py
@@ -0,0 +1,6 @@
+length = int(input('Введите целое число: '))
+breadth = int(input('Введите целое число: '))
+area = length*breadth
+print('Площадь равна',area)
+print('Периметр равен', 2*length+area)
+

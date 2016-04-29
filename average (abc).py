4bd841798a72c23eb38d1df4a9bf9fd5f4ffe8a4
spike23 <nickolay.osiniy@gmail.com>
2016-04-29 12:05:29 +0300

    pep8

--- average (abc).py
+++ average (abc).py
@@ -2,9 +2,9 @@
 a = int(input('Введите целое число: '))
 b = int(input('Введите целое число: '))
 c = int(input('Введите целое число: '))
-if a > b and a < c or a > c and a < b:
+if (a > b) and (a < c) or (a > c) and (a < b):
     print(a)
-elif b > a and b < c or b > c and b < a:
+elif (b > a) and (b < c) or (b > c) and (b < a):
     print(b)
 else:
     print(c)

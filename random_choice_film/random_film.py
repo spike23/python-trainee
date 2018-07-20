8122e85c53e352ef4fd485d461ea325f937da145
nickolay.osiniy <nickolay.osiniy@gmail.om>
2018-07-20 17:08:14 +0300

    add new file

--- random_choice_film/random_film.py
+++ random_choice_film/random_film.py
@@ -1,10 +1,18 @@
 import random
 
-my_films = open("C:\\Users\\admin\\Downloads\\films.txt").read().split('\n')
-filmlist = []
-a = int(input("How many films will you watch? "))
-for i in my_films:
-    filmlist.append(i)
-choice = random.sample(filmlist, a)
-result = ', '.join(choice)
-print(result)
+file_path = "C:\\Users\\admin\\Downloads\\films.txt"
+new_file = "C:\\Users\\admin\\Downloads\\download.txt"
+
+
+with open(file_path, 'r') as my_films, open(new_file, 'w') as new:
+    film_list = my_films.read().split('\n')
+    print('List contains ' + str(len(film_list)) + ' films.')
+    quantity = int(input("How many films will you watch? "))
+    choice = random.sample(film_list, quantity)
+    result = '\n'.join(choice)
+    print(result)
+    new.writelines(result)
+    #
+    update_list = [x for x in film_list if x not in choice]
+    new_film_list = ', '.join(update_list)
+
